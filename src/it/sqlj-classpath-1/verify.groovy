431be7ebcc7ca13f66aeb769751a52d01086a528
Anders Hammar <anders@hammar.net>
2017-09-07 10:18:01 +0200

    Site checkin for project Mojo's SQLJ Maven Plugin

--- src/it/sqlj-classpath-1/verify.groovy
+++
@@ -1,5 +0,0 @@
-// Verify that source and .ser files were generated
-assert new File( basedir, 'target/generated-sources/sqlj/test/com/tmme/x01/TestUNICODE.java' ).exists()
-assert new File( basedir, 'target/generated-sources/sqlj/test/com/tmme/x01/TestUNICODE2.java' ).exists()
-assert new File( basedir, 'target/generated-resources/sqlj/test/com/tmme/x01/TestUNICODE_SJProfile0.ser' ).exists()
-assert new File( basedir, 'target/generated-resources/sqlj/test/com/tmme/x01/TestUNICODE2_SJProfile0.ser' ).exists()
